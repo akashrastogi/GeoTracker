fa152cd63cfcc5b8e523ea68ccfd311db5189346
Akash Rastogi <ios@quippp.com>
2015-07-10 23:55:18 +0530

    Add map view to show user current location

--- LocationTracker_Test/ViewController.h
+++ LocationTracker_Test/ViewController.h
@@ -7,11 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UISwitch *switchLocationTracking;
 @property (weak, nonatomic) IBOutlet UISwitch *switchRegionMonitoring;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
--- LocationTracker_Test/ViewController.m
+++ LocationTracker_Test/ViewController.m
@@ -22,6 +22,7 @@
     // add action target to the controls
     [self.switchLocationTracking addTarget:self action:@selector(locationTrackingValueChanged:) forControlEvents:UIControlEventValueChanged];
     [self.switchRegionMonitoring addTarget:self action:@selector(regionMonitoringValueChanged:) forControlEvents:UIControlEventValueChanged];
+    self.mapView.delegate = self;
     
     [self initialSetup];
 }
@@ -52,5 +53,14 @@
     else [tracker stopRegionMonitoring];
     
     [Location setregionMonitoring:sender.isOn];
+}
+
+#pragma mark - MKMapView Delegate methods
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+}
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation{
+    return nil;
 }
 @end
